d657c67aeac1dd3047ab36dee18a263b9299b52e
Quentin Jungo <quentin.jungo@gmail.com>
2023-07-05 14:20:25 +0200

    add arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,45 @@
 #![allow(dead_code, unused_imports)]
+use std::sync::Mutex;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 mod state;
 mod update;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Run in development mode
+    #[arg(short, long)]
+    dev: bool,
+
+    #[arg(short, long, default_value = "8080")]
+    port: u16,
+
+    #[arg(long, default_value = "127.0.0.1")]
+    host: String,
+}
+
+fn main_dev() {
+    // update::update_request::test_to_json();
+    update::update_response::test_to_json();
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let arg = std::env::args().nth(1).unwrap_or("".to_string());
-    if arg == "--dev" {
-        // update::update_request::test_to_json();
-        update::update_response::test_to_json();
+    let args = Args::parse();
+    if args.dev {
+        main_dev();
         return Ok(());
     }
 
-    let state = web::Data::new(state::State {});
+    let state = web::Data::new(state::State {
+        ast: Mutex::new(vec![]),
+        project_path: Mutex::new(std::path::PathBuf::new()),
+    });
     HttpServer::new(move || App::new().app_data(state.clone()))
-        .bind(("127.0.0.1", 8080))?
+        .bind((args.host, args.port))?
         .run()
         .await
 }
--- src/state.rs
+++ src/state.rs
@@ -1,3 +1,7 @@
+use ast_node;
+use std::{path::PathBuf, sync::Mutex};
+
 pub struct State {
-    // counter: Mutex<i32>,
+    pub project_path: Mutex<PathBuf>,
+    pub ast: Mutex<Vec<ast_node::ASTNode>>,
 }
